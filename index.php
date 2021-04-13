2f3a55c4e52dc9bf32b835add92cd32dbe5e7677
Oliver <65659877+Snopy692@users.noreply.github.com>
2021-04-13 17:16:15 +0200

    Update index.php

--- index.php
+++ index.php
@@ -1,1 +1,14 @@
+<!DOCTYPE html>
+<html lang="pl">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>My first APP</title>
+</head>
+<body>
+<?php include "header.php"; ?>
 
+<?php include "footer.php"; ?>  
+</body>
+</html>
