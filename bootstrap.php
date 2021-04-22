ecc901896b2c8a5fefb7fbe2c8edf407ed74f05b
Oliver <65659877+Snopy692@users.noreply.github.com>
2021-04-22 13:10:35 +0200

    Fixed folder path

--- bootstrap.php
+++ bootstrap.php
@@ -1,9 +1,9 @@
 <?php
 require __DIR__ . '/vendor/autoload.php';
 
-require_once 'header.php';
+require_once 'core/views/region/header.tpl.php';
 require_once 'routing.php';
-require_once 'footer.php';
+require_once 'core/views/region/footer.tpl.php';
 
 ?>
 
