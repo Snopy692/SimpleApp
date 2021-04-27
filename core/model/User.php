256d95ef48378b2a6a324af27165601fb35f3772
Oliver <65659877+Snopy692@users.noreply.github.com>
2021-04-27 13:51:13 +0200

    Models
    Updated User.php

--- core/model/User.php
+++ core/model/User.php
@@ -14,7 +14,7 @@ class User
     private $data;
 
     private $status;
-
+// NAME
     public function setName($name)
     {
         $this->name = $name;
@@ -23,5 +23,35 @@ class User
     public function getName()
     {
         return $this->name;
+    }
+//PASSWORD
+    public function setPassword($password)
+    {
+        $this->password = $password;
+    }
+
+    public function getPassword()
+    {
+        return $this->password;
+    }
+ //DATA   
+    public function setData($data)
+    {
+        $this->data = $data;
+    }
+
+    public function getData()
+    {
+        return $this->data;
+    }
+//STATUS
+    public function setStatus($status)
+    {
+        $this->status = $status;
+    }
+
+    public function getStatus()
+    {
+        return $this->status;
     }
 }
