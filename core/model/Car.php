1982c149438a626d6866ec4e1f704ed2d6866035
Oliver <65659877+Snopy692@users.noreply.github.com>
2021-05-03 10:33:50 +0200

    Client/Car.php
    Client.php - done
    Car.php - to be continued

---
+++ core/model/Car.php
@@ -0,0 +1,151 @@
+<?php
+
+namespace App/Model
+
+/**
+* Class Car
+*
+* The Car model class sets & gets user data from the database and returns it to user.
+*/
+class Car 
+
+{
+    /**
+     * @var integer
+     */
+    private $id;
+
+    /**
+     * @var string
+     */
+    private $fullName;
+
+    /**
+     * @var integer
+     */
+    private $segment;
+
+    /**
+     * @var integer
+     */
+    private $carType;
+
+    /**
+     * @var integer
+     */
+    private $fuelType;
+
+    /**
+     * @var integer
+     */
+    private $transmissionType;
+
+    /**
+     * @var integer
+     */
+    private $seatsNumber;
+
+    /**
+     * @var float
+    */
+    private $fuelConsumption;
+
+    /**
+     * @var integer
+     */
+    private $year;
+
+    /**
+     * @var float
+     */
+    private $acceleration;
+
+    /**
+     * @var integer
+     */
+    private $engineSize;
+
+    /**
+     * @var integer
+     */
+    private $power;
+
+    /**
+     * @var integer
+     */
+    private $trunkCapacity;
+
+    /**
+     * @var integer
+     */
+    private $minDriverAge;
+
+    /**
+     * Getter for id.
+     * 
+     * @var integer
+     */
+    public function getId() :int
+    {
+        return $id;
+    }
+
+    /**
+     * Setter for fullName
+     * 
+     * @param string $fullName
+     */
+    public function setFullName(string $fullName)
+    {
+        $this->fullName = $fullName;
+    }
+
+    /**
+     * Getter for fullName
+     */
+    public function getFullName() :string
+    {
+        return $this->fullName;
+    }
+
+    /**
+     * Setter for segment
+     * 
+     * @param integer $segment
+     */
+    public function setSegment(int $segment)
+    {
+        $this->segment = $segment;
+    }
+
+    /**
+     * Getter for segment.
+     * 
+     * @var int
+     */
+    public function getSegment() :int
+    {
+        return $this->segment;
+    }
+
+    /**
+     * Setter for carType
+     * 
+     * @param int $carType
+     */
+    public function setCarType(int $carType)
+    {
+        $this->carType = $carType;
+    }
+
+    /**
+     * Getter for carType
+     * 
+     * @var integer
+     */
+    public function getCarType() :int
+    {
+        return $this->carType;
+    }
+
+}
--- core/model/Client.php
+++ core/model/Client.php
@@ -69,52 +69,229 @@ namespace App\Model;
         */
         private $vatNumber;
 
-
+        /**
+         * Getter for id.
+         * 
+         * @return int
+         */
         public function getId() :int
         {
             return $id;
         }
-
+        
+        /**
+         * Setter for uid.
+         * 
+         * @param integer $uid
+         */
         public function setUid(int $uid)
         {
             $this->uid = $uid;
         }
 
+        /**
+         * Getter for uid.
+         * 
+         * @var integer
+         */
         public function getUid() :int
         {
             return $this->uid;
         }
 
+        /**
+         * Setter for email.
+         * 
+         * @param string $email
+         */
         public function setEmail(string $email)
         {
             $this->email = $email;
         }
 
+        /**
+         * Getter for email.
+         * 
+         * @var string
+         */
         public function getEmail() :string
         {
             return $this->email;
         }        
 
-        
+        /**
+         * Setter for firstName.
+         * 
+         * @param string $firstName
+         */
         public function setFirstName(string $firstName)
         {
             $this->firstName = $firstName;
         }
 
+        /**
+         * Getter for firstName.
+         * 
+         * @var string
+         */
         public function getFirstName() :string
         {
             return $this->firstName;
         }   
 
+        /**
+         * Setter for lastName.
+         * 
+         * @param string $lastName
+         */
         public function setLastName(string $lastName)
         {
             $this->firstName = $lastName;
         }
 
-        public function getLirstName() :string
+        /**
+         * Getter for lastName.
+         * 
+         * @var string
+         */
+        public function getLastName() :string
         {
             return $this->lastName;
-        } 
+        }
+        
+        /**
+         * Setter foor company.
+         * 
+         * @param string $company
+         */
+        public function setCompany(string $company)
+        {
+            $this->company = $company;
+        }
+        
+        /**
+         * Getter for company
+         * 
+         * @var string
+         */
+        public function getCompany() :string
+        {
+            return $this->company;
+        }
+
+        /**
+         * Setter for street.
+         * 
+         * @param string $street
+         */
+        public function setStreet(string $street)
+        {
+            $this->street = $street;
+        }
+
+        /**
+         * Getter for street.
+         * 
+         * @var string
+         */
+        public function getStreet() :string
+        {
+            return $this->street;
+        }
+
+        /**
+         * Setter for houseNumber.
+         * 
+         * @param string $houseNumber
+         */
+        public function setHouseNumber(string $houseNumber)
+        {
+            $this->houseNumber = $houseNumber;
+        }
+
+        /**
+         * Getter for houseNumber
+         * 
+         * @var string
+         */
+        public function getHouseNumber() :string
+        {
+            return $this->houseNumber;
+        }
+
+        /**
+         * Setter for flatNumber
+         * 
+         * @param string $flatNumber
+         */
+        public function setFlatNumber(string $flatNumber)
+        {
+            $this->flatNumber = $flatNumber;
+        }
+
+        /**
+         * Getter for flatNumber
+         * 
+         * @var string
+         */
+        public function getFlatNumber() :string
+        {
+            return $this->flatNumber;
+        }
+        
+        /**
+         * Setter for zipCode
+         * 
+         * @param string $zipCode
+         */
+        public function setZipCode(string $zipCode)
+        {
+            $this->zipCode = $zipCode;
+        }
+
+        /**
+         * Getter for zipCode.
+         * 
+         * @var string
+         */
+        public function getZipCode() :string
+        {
+            return $this->zipCode;
+        }
+
+        /**
+         * Setter for city.
+         * 
+         * @param string $city
+         */
+        public function setCity(string $city)
+        {
+            $this->city = $city;
+        }
+
+        /**
+         * Getter for city
+         * 
+         * @var string
+         */
+        public function getCity() :string
+        {
+            return $this->city;
+        }
+
+        /**
+         * Setter for vatNumber.
+         * 
+         * @param string $vatNumber
+         */
+        public function setVatNumber(int $vatNumber)
+        {
+            $this->vatNumber = $vatNumber;
+        }
+
+        public function getVatNumber() :int
+        {
+            return $this->vatNumber;
+        }
 
    }
-?>
